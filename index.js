00a0f06d007b26d8be8218028e65073478faa651
kelvin-kariuki <Kariuki405@gmail.com>
2022-06-17 14:43:16 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(name,email) {
+    const users ={uName:name, uEmail: email}
+    return fetch('http://localhost:3000/users',{
+        method: "POST",
+        Headers: {
+            "Content-Type":"application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(users)
+
+    })
+    .then(resp => resp.json())
+    .then(data => {document.body.innerHTML = data.id})
+    //     method: "POST",
+    //     Headers: {
+    //         "Content-Type":"application/json",
+    //     Accept: "application/json",
+    // },
+    //     body: JSON.stringify()
+    // })
+    // .then(resp => resp.json())
+    // .then(data => {document.innerHTML = data.id})
+    .catch(error => {
+        document.body.innerHTML = error.message;
+        alert("Invalid name or email");
+    });
+
+
+}
+
+// The program accepts username and email
+
